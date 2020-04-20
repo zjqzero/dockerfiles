95868de9f91f5cc67ff08b3076835aff28f46e80
Jonas <zjqzero@163.com>
2020-04-20 23:25:20 +0800

    [zjq]: add cython build env

--- cython/build.sh
+++ cython/build.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-old_dir=`pwd`
-dir="$1"
-
-cd $1
+cd /to_build
 
 python3 setup.py build_ext --inplace
 
-find . -name "*.py" -exec rm -rf {} \;
-find . -name "*.c" -exec rm -rf {} \;
+find ./app -name "*.py" -exec rm -rf {} \;
+find ./app -name "*.c" -exec rm -rf {} \;
 rm -rf build
 
 for src_file in `find . -name "*.so"`; do
   dst_file=`echo $src_file | sed -e 's/\.cpython.*gnu//g'`;
   mv $src_file $dst_file; 
 done
-
-
-cd $old_dir
