9f5b8c41ed58ed659d5137d76e9b8257eb9b128d
Jonas <zjqzero@163.com>
2020-04-21 09:18:58 +0800

    [zjq]: update

--- cython/build.sh
+++ cython/build.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
+remove_files() {
+    file_path=$1
+    ext_type=$2
+    find $1 -name "$2" -exec rm -rf {} \;
+}
+
+remove_commects() {
+    file_path=$1
+    find $1 -name "*.c" -exec sed -i "s/\/\*.*\*\///" {} \;
+    find $1 -name "*.c" -exec sed -i "/^[ \t]*\/\*/,/.*\*\//d" {} \;
+}
+
+rename_so_files() {
+    for src_file in `find . -name "*.so"`; do
+      dst_file=`echo $src_file | sed -e 's/\.cpython.*gnu//g'`;
+      mv $src_file $dst_file; 
+    done
+}
+
+
 cd /to_build
 
 python3 setup.py build_ext --inplace
 
-find ./app -name "*.py" -exec rm -rf {} \;
-find ./app -name "*.c" -exec rm -rf {} \;
-rm -rf build
+remove_files ./app *.py
+remove_files ./app *.c
+rename_so_files
 
-for src_file in `find . -name "*.so"`; do
-  dst_file=`echo $src_file | sed -e 's/\.cpython.*gnu//g'`;
-  mv $src_file $dst_file; 
-done
+rm -rf build
